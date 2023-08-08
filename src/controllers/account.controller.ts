f7e59ab53b9519c5d8bb99a5383d5c82c0ae1b8d
maiquocthinh <majquocthjnh@gmail.com>
2023-08-08 22:35:20 +0700

    feat: add & setup router for forgot & reset-password pages

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -15,6 +15,14 @@ export const logout = async (req: Request, res: Response) => {
 	return res.status(200).json({ msg: 'logout' });
 };
 
+export const forgot = async (req: Request, res: Response) => {
+	return res.status(200).render('forgot');
+};
+
+export const resetPassword = async (req: Request, res: Response) => {
+	return res.status(200).render('reset-password');
+};
+
 export const panel = async (req: Request, res: Response) => {
 	return res.status(200).render('panel');
 };
--- src/routes/account.router.ts
+++ src/routes/account.router.ts
@@ -2,13 +2,15 @@ import { Router, Request, Response } from 'express';
 
 import { accountController } from '@controllers/index';
 
-const { login, register, logout, panel } = accountController;
+const { login, register, logout, forgot, resetPassword, panel } = accountController;
 
 const router = Router();
 
 router.get('/login', login);
 router.get('/register', register);
 router.get('/logout', logout);
+router.get('/forgot', forgot);
+router.get('/reset-password', resetPassword);
 router.get('/panel', panel);
 router.get('*', (req: Request, res: Response) => {
 	return res.redirect('/account/panel');
