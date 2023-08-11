19d549efb56ca321be42e468852aecb7aa4ede7d
maiquocthinh <majquocthjnh@gmail.com>
2023-08-11 22:04:05 +0700

    feat: add captcha

--- src/controllers/account.controller.ts
+++ src/controllers/account.controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import svgCaptcha, { CaptchaObj } from 'svg-captcha';
+import bcrypt from 'bcryptjs';
 
 // ╔════════════╗
 // ║	PAGE	║
@@ -25,4 +27,23 @@ export const resetPassword = async (req: Request, res: Response) => {
 
 export const panel = async (req: Request, res: Response) => {
 	return res.status(200).render('panel');
+};
+
+export const captcha = async (_req: Request, res: Response) => {
+	const captcha: CaptchaObj = svgCaptcha.create({
+		size: 4,
+		ignoreChars: '0o1i',
+		noise: 2,
+	});
+
+	// ignore cache svg
+	res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+	res.setHeader('Pragma', 'no-cache');
+	res.setHeader('Expires', '0');
+
+	// save captcha after hash to client
+	const hashedCaptcha = bcrypt.hashSync(captcha.text, 10);
+	res.cookie('cc_hash', hashedCaptcha, { httpOnly: true, maxAge: 900000 }); // 15m
+
+	return res.type('svg').send(captcha.data);
 };
--- src/routes/account.router.ts
+++ src/routes/account.router.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 
 import { accountController } from '@controllers/index';
 
-const { login, register, logout, forgot, resetPassword, panel } = accountController;
+const { login, register, logout, forgot, resetPassword, panel, captcha } = accountController;
 
 const router = Router();
 
@@ -12,6 +12,7 @@ router.get('/logout', logout);
 router.get('/forgot', forgot);
 router.get('/reset-password', resetPassword);
 router.get('/panel', panel);
+router.get('/captcha', captcha);
 router.get('*', (req: Request, res: Response) => {
 	return res.redirect('/account/panel');
 });
